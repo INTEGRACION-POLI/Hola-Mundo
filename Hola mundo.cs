fab9fb9b3abd608b56d8ade9695aedfc174b6a69
jaimesantana2 <jsantana25@misena.edu.co>
2018-05-28 23:28:52 -0500

    Create Hola mundo.cs

---
+++ Hola mundo.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+
+public class HolaMundo : MonoBehaviour {
+
+public void imprimir()
+    {
+        Console.WriteLine("Hola Mundo");
+    }
+}
